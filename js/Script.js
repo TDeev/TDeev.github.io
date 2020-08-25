11d4d9963ffc369e9ed223105db400b55b70cbe8
Timur Deev <deev.timur@gmail.com>
2020-08-25 17:28:47 +0500

    Залил проект интеллекта

--- js/Script.js
+++ js/Script.js
@@ -1,60 +1,64 @@
-// Шапка
-let header = document.querySelector('.header-nav');
-let header_logo = document.querySelector('.header-nav-logo-svg');
-let header_logo_name = document.querySelector('.header-nav-logo-txt');
-let header_url = document.querySelectorAll('.header-nav-list-items-item-url');
-let header_basket = document.querySelector('.header-nav-basket-svg');
-let header_search = document.querySelector('.header-nav-search-form-label-svg');
-let header_search_input = document.querySelector('.header-nav-search-form-input');
-let header_menu = document.querySelector('.header-nav-menu-label-line');
+//============ЛЕВОЕ МЕНЮ============
 
-window.onscroll = function () {
-  if (window.pageYOffset == 0) {
-    header.classList.remove('scroll');
-    header_logo.classList.remove('color');
-    header_logo_name.classList.remove('color');
-    header_url[0].classList.remove('color');
-    header_url[1].classList.remove('color');
-    header_url[2].classList.remove('color');
-    header_url[3].classList.remove('color');
-    header_url[4].classList.remove('color');
-    header_url[5].classList.remove('color');
-    header_url[6].classList.remove('color');
-    header_url[7].classList.remove('color');
-    header_basket.classList.remove('color');
-    header_search.classList.remove('color');
-    header_search_input.classList.remove('br-color');
-    header_menu.classList.remove('bc-color');
-  }
-  else {
-    header.classList.add('scroll');
-    header_logo.classList.add('color');
-    header_logo_name.classList.add('color');
-    header_url[0].classList.add('color');
-    header_url[1].classList.add('color');
-    header_url[2].classList.add('color');
-    header_url[3].classList.add('color');
-    header_url[4].classList.add('color');
-    header_url[5].classList.add('color');
-    header_url[6].classList.add('color');
-    header_url[7].classList.add('color');
-    header_basket.classList.add('color');
-    header_search.classList.add('color');
-    header_search_input.classList.add('br-color');
-    header_menu.classList.add('bc-color');
+//элемент левой меню
+let navParent = document.querySelectorAll('.navbar-menu-list-item-content');
+
+for (let i = 0; i < navParent.length; i++) {
+  navParent[i].onclick = function () {
+    navParent[i].classList.toggle('open');
   }
 }
 
-// Видео
-let video = document.querySelector('.presentation-video'),
-  button = document.querySelector('.presentation-content-btn'),
-  content = document.querySelector('.presentation-content');
+//============ФИЛЬТР============
 
-button.addEventListener("click", function () {
-  video.play()
-  video.setAttribute("controls", "controls");
-}, false);
+//блок фильтра
+let filter = document.querySelector('.main-filter');
 
-button.onclick = function () {
-  content.classList.add('hiden');
+//кнопка фильтра
+let filterButton = document.querySelector('.main-controls-actions-filter');
+
+//иконка закрытия фильтра
+let filterClose = document.querySelector('.main-filter-header-close');
+
+//Открытие и закрытие фильтра
+filterButton.onclick = function () {
+  filter.classList.toggle('open');
+}
+
+//Закрытие фильтра
+filterClose.onclick = function () {
+  filter.classList.remove('open');
+}
+
+//============ЛЕВОЕ МЕНЮ НАВИГАЦИИ============
+
+let body = document.querySelector('body');
+
+let navButton = document.querySelector('.navbar-head-btn');
+
+
+//нажатие на кнопку навигации
+
+navButton.onclick = function () {
+  body.classList.toggle('navbar-close');
+  // console.log('Салам!');
+}
+
+//============РАЗМЕР ЭКРАНА============
+
+//определение размера экрана
+if (innerWidth <= 992) {
+  body.classList.add('navbar-close');
+}
+
+//отслеживание изменения размера окна браузера
+window.onresize = function () {
+  // alert('Размер окна был изменен!');
+
+  if (window.innerWidth <= 992) {
+    body.classList.add('navbar-close');
+  }
+  else {
+    body.classList.remove('navbar-close');
+  }
 }
