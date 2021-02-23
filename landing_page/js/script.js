f323ea5d24a0619d86600fa4d80b2350894c992e
Òèìóð Äååâ <deev.timur@gmail.com>
2021-02-23 17:13:19 +0500

    шапка и видео

---
+++ landing_page/js/script.js
@@ -0,0 +1,27 @@
+//*Шапка
+// let header = document.getElementsByClassName('header-nav');
+let header = document.querySelector('.header-nav');
+
+window.onscroll = function () {
+  if (window.pageYOffset == 0) {
+    header.classList.remove('top');
+  }
+  else {
+    header.classList.add('header-nav--top');
+  }
+}
+
+
+//*Видео
+let video = document.querySelector('.presentation-video'), // видео файл
+  button = document.querySelector('.presentation-btn'); // кнопка
+// content = document.querySelector('.presentation-content'); // контент, который нужно скрыть
+
+button.addEventListener("click", function () {
+  video.play()
+  video.setAttribute("controls", "controls");
+}, false);
+
+button.onclick = function () {
+  button.classList.add('presentation-btn--hiden');
+}
